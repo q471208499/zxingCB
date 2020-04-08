5ad6d75c251be277c5817828da1ac71ce6b8297f
chenb <471208499@qq.com>
2020-04-08 15:04:51 +0800

    migrate to androidX

--- app/src/main/java/com/yzq/zxing/FragmentActivity.java
+++ app/src/main/java/com/yzq/zxing/FragmentActivity.java
@@ -2,11 +2,10 @@ package com.yzq.zxing;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
-import android.widget.RelativeLayout;
 
 import com.yzq.zxinglibrary.common.Constant;
 
